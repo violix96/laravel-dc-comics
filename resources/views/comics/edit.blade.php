d5bd84a32c8519fade360c57058d3d562c52c424
Vincenzo Fabrizio Viola <vincy1621@gmail.com>
2024-07-15 16:40:33 +0200

    [UPD] set value in edit form

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -19,49 +19,58 @@
                             <div class="mb-3">
                                 <label class="form-text" for="exampleFormControlInput1" class="form-label">Nome
                                     fumetto</label>
-                                <input type="text" class="form-control" name="title">
+                                <input type="text" class="form-control" name="title"
+                                    value="{{ old('title', $comic->title) }}">
                             </div>
                             <div class="mb-3">
                                 <label class="form-text" for="exampleFormControlInput1"
                                     class="form-label">Descrizione</label>
-                                <input type="text" class="form-control" name="description">
+                                <input type="text" class="form-control" name="description"
+                                    value="{{ old('description', $comic->description) }}">
                             </div>
                             <div class="mb-3">
                                 <label class="form-text" for="exampleFormControlInput1" class="form-label">Inserisci
                                     immagine</label>
-                                <input type="text" class="form-control" name="thumb">
+                                <input type="text" class="form-control" name="thumb"
+                                    value="{{ old('thumb', $comic->thumb) }}">
                             </div>
                             <div class="mb-3">
                                 <label class="form-text" for="exampleFormControlInput1" class="form-label">Prezzo</label>
-                                <input type="text" class="form-control" name="price">
+                                <input type="text" class="form-control" name="price"
+                                    value="{{ old('price', $comic->price) }}">
                             </div>
                             <div class="mb-3">
                                 <label class="form-text" for="exampleFormControlInput1" class="form-label">Serie</label>
-                                <input type="text" class="form-control" name="series">
+                                <input type="text" class="form-control" name="series"
+                                    value="{{ old('series', $comic->series) }}">
                             </div>
                         </div>
                         <div class="col-6">
                             <div class="mb-3">
                                 <label class="form-text" for="exampleFormControlInput1" class="form-label">Data di
                                     vendita</label>
-                                <input type="date" class="form-control" name="sale_date">
+                                <input type="date" class="form-control" name="sale_date"
+                                    value="{{ old('sale_date', $comic->sale_date) }}">
                             </div>
                             <div class="mb-3">
                                 <label class="form-text" for="exampleFormControlInput1" class="form-label">Genere</label>
-                                <input type="text" class="form-control" name="type">
+                                <input type="text" class="form-control" name="type"
+                                    value="{{ old('type', $comic->type) }}">
                             </div>
                             <div class="mb-3">
                                 <label class="form-text" for="exampleFormControlInput1" class="form-label">Autori</label>
-                                <input type="text" class="form-control" name="artists">
+                                <input type="text" class="form-control" name="artists"
+                                    value="{{ old('artists', implode(', ', json_decode($comic->artists, true))) }}">
                             </div>
                             <div class="mb-3">
                                 <label class="form-text" for="exampleFormControlInput1" class="form-label">Scrittori</label>
-                                <input type="text" class="form-control" name="writers">
+                                <input type="text" class="form-control" name="writers"
+                                    value="{{ old('writers', implode(', ', json_decode($comic->writers, true))) }}">
                             </div>
                         </div>
                     </div>
 
-                    <button class="btn btn-primary">Crea fumetto</button>
+                    <button class="btn btn-primary">Modifica fumetto</button>
                     <div class="my-5">
                         <a class="btn btn-primary text-white" href="{{ route('comics.index') }}">Torna alla Home</a>
                     </div>
